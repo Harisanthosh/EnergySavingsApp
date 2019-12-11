a536b95947d0cb7f9f248f901aefb8f0d21c3b63
Hari Santhosh <harisanthosh94@gmail.com>
2019-12-11 20:22:39 +0530

    API Changes for plotting

--- energy_api.py
+++ energy_api.py
@@ -1,8 +1,33 @@
 import sqlite3
-conn = sqlite3.connect('energy.db')
+import matplotlib.pyplot as plt
+import sys
 
-c = conn.cursor()
 
-c.execute("SELECT * FROM dataRM")
+def run_query(table):
+    conn = sqlite3.connect('energy.db')
 
-print(c.fetchall())
+    c = conn.cursor()
+
+    c.execute("SELECT processTime,"+ table +" FROM dataRM")
+
+    data = c.fetchall()
+
+    print(data)
+    processArr = []
+    avgArr = []
+
+    for row in data:
+        processArr.append(row[0])
+        avgArr.append(row[1])
+
+    plt.xlabel('Process Time')
+    plt.ylabel('Energy Consumption Signal')
+    plt.title('Energy Database Overview')
+    plt.plot(processArr,avgArr)
+    plt.show()
+
+if __name__ == "__main__":
+    table_name = sys.argv[1] if len(sys.argv) > 1  else "energyAve_kW"
+    run_query(table_name)
+
+
