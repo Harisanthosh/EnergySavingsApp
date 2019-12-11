4816f0deec0f6c62b12146e758abf18840d91bf9
Hari Santhosh <harisanthosh94@gmail.com>
2019-12-11 20:48:51 +0530

    Multi part graphs

--- energy_api.py
+++ energy_api.py
@@ -8,26 +8,37 @@ def run_query(table):
 
     c = conn.cursor()
 
-    c.execute("SELECT processTime,"+ table +" FROM dataRM")
+    c.execute("SELECT processTime,energyMin_kW,energyAve_kW,energyMax_kW FROM "+ table)
 
     data = c.fetchall()
 
     print(data)
     processArr = []
+    minArr = []
     avgArr = []
+    maxArr = []
+    totalArr = [minArr, avgArr, maxArr]
+    cr = ['r', 'b', 'g', 'y', 'p']
+    rowcolor = 0
 
     for row in data:
         processArr.append(row[0])
-        avgArr.append(row[1])
+        minArr.append(row[1])
+        avgArr.append(row[2])
+        maxArr.append(row[3])
 
     plt.xlabel('Process Time')
     plt.ylabel('Energy Consumption Signal')
     plt.title('Energy Database Overview')
-    plt.plot(processArr,avgArr)
+    for t in totalArr:
+        plt.plot(processArr, t, color=cr[rowcolor])
+        plt.legend(loc='upper left')
+        rowcolor += 1
+
     plt.show()
 
 if __name__ == "__main__":
-    table_name = sys.argv[1] if len(sys.argv) > 1  else "energyAve_kW"
+    table_name = sys.argv[1] if len(sys.argv) > 1  else "dataRM"
     run_query(table_name)
 
 
