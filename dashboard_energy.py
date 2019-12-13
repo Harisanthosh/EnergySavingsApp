20119d788164f989611aaa1d6091a194de4699e3
Hari Santhosh <harisanthosh94@gmail.com>
2019-12-13 15:56:56 +0530

    Dynamic text update to fetch records

--- dashboard_energy.py
+++ dashboard_energy.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import sqlite3
 #from plotly.offline import init_notebook_mode, plot_mpl
 from plotly.tools import mpl_to_plotly
@@ -47,15 +48,62 @@ for t in totalArr:
 plotly_fig = mpl_to_plotly(fig)
 #plt.show()
 
+
+
 app.layout = html.Div(children=[
     html.H1(children='Energy Dashboard'),
 
-    html.Div(children='''
+    html.Div(id="energy-table",children='''
         Energy Dashboard for PPS Systems
     '''),
 
-    dcc.Graph(id='myGraph', figure=plotly_fig)
+    dcc.Graph(id='myGraph', figure=plotly_fig),
+    html.Br(),
+    dcc.Input(
+            id="input_text".format("text"),
+            type="text",
+            placeholder="input type text".format("text"),
+        ),
+    html.Button('Get Energy Table', id='button_neo',style={'color': '#D4AF37'})
 ])
+
+@app.callback(Output("energy-table", "children"),[Input("input_text", "value")])
+def update_energy_table(value):
+    conn = sqlite3.connect('energy.db')
+
+    c = conn.cursor()
+
+    c.execute("SELECT processTime,energyMin_kW,energyAve_kW,energyMax_kW FROM "+value)
+
+    data = c.fetchall()
+
+    print(data)
+    processArr = []
+    minArr = []
+    avgArr = []
+    maxArr = []
+    totalArr = [minArr, avgArr, maxArr]
+    cr = ['r', 'b', 'g', 'y', 'p']
+    tr = ['energyMin_kW', 'energyAve_kW', 'energyMax_kW']
+    rowcolor = 0
+
+    for row in data:
+        processArr.append(row[0])
+        minArr.append(row[1])
+        avgArr.append(row[2])
+        maxArr.append(row[3])
+
+    fig = plt.figure()
+    plt.xlabel('Process Time')
+    plt.ylabel('Energy Consumption Signal')
+    plt.title('Energy Database Overview')
+    for t in totalArr:
+        plt.plot(processArr, t, color=cr[rowcolor], label=tr[rowcolor])
+        plt.legend(loc='lower left')
+        rowcolor += 1
+
+    plotly_fig = mpl_to_plotly(fig)
+    # plt.show()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
