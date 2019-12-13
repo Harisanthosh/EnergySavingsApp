9382d245b0b64bf28d7b6717b7ccc24288a0abad
Hari Santhosh <harisanthosh94@gmail.com>
2019-12-13 15:09:24 +0530

    Plotly compatabile matplotlib conversion

---
+++ dashboard_energy.py
@@ -0,0 +1,61 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import sqlite3
+#from plotly.offline import init_notebook_mode, plot_mpl
+from plotly.tools import mpl_to_plotly
+import matplotlib.pyplot as plt
+
+#graph = dcc.Graph(id='myGraph', fig=plotly_fig)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+conn = sqlite3.connect('energy.db')
+
+c = conn.cursor()
+
+c.execute("SELECT processTime,energyMin_kW,energyAve_kW,energyMax_kW FROM dataRM")
+
+data = c.fetchall()
+
+print(data)
+processArr = []
+minArr = []
+avgArr = []
+maxArr = []
+totalArr = [minArr, avgArr, maxArr]
+cr = ['r', 'b', 'g', 'y', 'p']
+tr= ['energyMin_kW','energyAve_kW','energyMax_kW']
+rowcolor = 0
+
+for row in data:
+    processArr.append(row[0])
+    minArr.append(row[1])
+    avgArr.append(row[2])
+    maxArr.append(row[3])
+
+fig = plt.figure()
+plt.xlabel('Process Time')
+plt.ylabel('Energy Consumption Signal')
+plt.title('Energy Database Overview')
+for t in totalArr:
+    plt.plot(processArr, t, color=cr[rowcolor], label=tr[rowcolor])
+    plt.legend(loc='lower left')
+    rowcolor += 1
+
+plotly_fig = mpl_to_plotly(fig)
+#plt.show()
+
+app.layout = html.Div(children=[
+    html.H1(children='Energy Dashboard'),
+
+    html.Div(children='''
+        Energy Dashboard for PPS Systems
+    '''),
+
+    dcc.Graph(id='myGraph', figure=plotly_fig)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
