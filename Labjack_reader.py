d86e3a305d434b36191931619eb33eac4224f74e
Hari Santhosh <harisanthosh94@gmail.com>
2020-01-08 21:11:59 +0530

    Labjack reader added to continuously fetch data

--- Labjack_reader.py
+++ Labjack_reader.py
@@ -18,12 +18,17 @@ results = ljm.eReadNames(handle, numFrames, names)
 
 print("\neReadNames results: ")
 for i in range(numFrames):
-    print("Measured Power of - %s, value : %f" % (names[i], (results[i]/0.1) * 9))
+    print("Measured Power of - %s, value : %f" % (names[i], (abs(results[i])/0.1) * 9))
     # print("Voltage of - %s, value : %f" % (names[i], results[i]))
 
-# regx = ['AIN0','AIN1','AIN2','AIN3']
+# while True:
+#     numFrames = 4
+#     # names = ["SERIAL_NUMBER", "PRODUCT_ID", "FIRMWARE_VERSION"]
+#     names = ['AIN0', 'AIN1', 'AIN2', 'AIN3']
+#     results = ljm.eReadNames(handle, numFrames, names)
 #
-# for trx in regx:
-#     rxx = ljm.eReadName(handle, trx)
-#     print("%s Voltage is %f" % (trx, rxx))
+#     print("\neReadNames results: ")
+#     for i in range(numFrames):
+#         print("Measured Power of - %s, value : %f" % (names[i], (abs(results[i]) / 0.1) * 9))
+
 
